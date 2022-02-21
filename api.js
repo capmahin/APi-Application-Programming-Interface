5b6dde37768356a7700da15fbb0df547f328d3b4
capmahin <mahinulhuque1996@gmail.com>
2022-02-22 04:46:51 +0600

    use post and users inonlick

--- api.js
+++ api.js
@@ -3,3 +3,18 @@ function loadData(){
   .then(response => response.json())
   .then(json => console.log(json))
 }
+
+function loadUsers(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(json => displayUsers(json));
+}
+
+function loadPosts(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then (res => res.json())
+    .then (json => console.log(json));
+}
+function displayUsers(json){
+    console.log(json)
+}
